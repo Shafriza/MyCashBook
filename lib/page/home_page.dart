6d7a2ef1947ee6c8c6351a0a092ca1906e542999
shafriza <sahurgaming@gmail.com>
2023-09-26 21:30:33 +0700

    add menu

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -72,115 +72,219 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
+        child: ListView(
           children: [
+            const SizedBox(height: 20),
             Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Penghasilan Bulan Ini",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  const SizedBox(height: 20),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: Card(
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Color.fromARGB(255, 221, 44, 44),
-                              backgroundImage:
-                                  AssetImage('assets/icon/expenses.png'),
-                            ),
-                            title: Text(
-                              "Pengeluaran",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 12),
-                            ),
-                            subtitle: Text(
-                              "Rp. 50.000",
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                        ),
+              child: const Text(
+                "Penghasilan Bulan Ini",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 221, 44, 44),
+                        backgroundImage: AssetImage('assets/icon/expenses.png'),
                       ),
-                      Expanded(
-                        child: Card(
-                          child: ListTile(
-                            leading: CircleAvatar(
-                              backgroundColor: Color.fromARGB(255, 16, 187, 59),
-                              backgroundImage:
-                                  AssetImage('assets/icon/profits.png'),
-                            ),
-                            title: Text(
-                              "Pendapatan",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 12),
-                            ),
-                            subtitle: Text(
-                              "Rp. 100.000",
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                        ),
+                      title: Text(
+                        "Pengeluaran",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12),
+                      ),
+                      subtitle: Text(
+                        "Rp. 50.000",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 16, 187, 59),
+                        backgroundImage: AssetImage('assets/icon/profits.png'),
+                      ),
+                      title: Text(
+                        "Pendapatan",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12),
+                      ),
+                      subtitle: Text(
+                        "Rp. 100.000",
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Builder(
+              builder: (context) {
+                return Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      width: 1.0,
+                      color: Colors.grey[400]!,
+                    ),
+                  ),
+                  height: 300,
+                  // color: Theme.of(context).cardColor,
+                  padding: const EdgeInsets.all(2.0),
+                  child: SfCartesianChart(
+                    title: ChartTitle(
+                      text: 'Grafik',
+                      textStyle: const TextStyle(fontSize: 12),
+                      alignment: ChartAlignment.center,
+                    ),
+                    legend: const Legend(
+                        isVisible: true, position: LegendPosition.bottom),
+                    primaryXAxis: DateTimeAxis(
+                      // Atur format tanggal sesuai dengan preferensi Anda
+                      dateFormat: DateFormat.E(),
+                    ),
+                    series: <ChartSeries>[
+                      LineSeries<Map<String, dynamic>, DateTime>(
+                        name: 'Pendapatan',
+                        dataSource: _pendapatan,
+                        color: Colors.green,
+                        xValueMapper: (Map<String, dynamic> data, _) =>
+                            data["date"],
+                        yValueMapper: (Map<String, dynamic> data, _) =>
+                            data["sales"],
+                      ),
+                      LineSeries<Map<String, dynamic>, DateTime>(
+                        name: 'Pengeluaran',
+                        dataSource: _pengeluaran,
+                        color: Colors.red,
+                        xValueMapper: (Map<String, dynamic> data, _) =>
+                            data["date"],
+                        yValueMapper: (Map<String, dynamic> data, _) =>
+                            data["sales"],
                       ),
                     ],
                   ),
-                  Builder(
-                    builder: (context) {
-                      return Container(
-                        margin: const EdgeInsets.only(top: 15),
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          borderRadius: BorderRadius.circular(8),
-                          border: Border.all(
-                            width: 1.0,
-                            color: Colors.grey[400]!,
-                          ),
-                        ),
-                        height: 300,
-                        // color: Theme.of(context).cardColor,
-                        padding: const EdgeInsets.all(8.0),
-                        child: SfCartesianChart(
-                          title: ChartTitle(
-                            text: 'Grafik',
-                            textStyle: const TextStyle(fontSize: 12),
-                            alignment: ChartAlignment.center,
-                          ),
-                          legend: const Legend(
-                              isVisible: true, position: LegendPosition.bottom),
-                          primaryXAxis: DateTimeAxis(
-                            // Atur format tanggal sesuai dengan preferensi Anda
-                            dateFormat: DateFormat.E(),
-                          ),
-                          series: <ChartSeries>[
-                            LineSeries<Map<String, dynamic>, DateTime>(
-                              name: 'Pendapatan',
-                              dataSource: _pendapatan,
-                              color: Colors.green,
-                              xValueMapper: (Map<String, dynamic> data, _) =>
-                                  data["date"],
-                              yValueMapper: (Map<String, dynamic> data, _) =>
-                                  data["sales"],
-                            ),
-                            LineSeries<Map<String, dynamic>, DateTime>(
-                              name: 'Pengeluaran',
-                              dataSource: _pengeluaran,
-                              color: Colors.red,
-                              xValueMapper: (Map<String, dynamic> data, _) =>
-                                  data["date"],
-                              yValueMapper: (Map<String, dynamic> data, _) =>
-                                  data["sales"],
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                "Menu",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 4,
+              padding: const EdgeInsets.all(4),
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+              children: [
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/icon/income.png',
+                          height: 40,
+                        ),
+                        const Text(
+                          "Income",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/icon/outcome.png',
+                          height: 40,
+                        ),
+                        const Text(
+                          "Outcome",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/icon/details.png',
+                          height: 40,
+                        ),
+                        const Text(
+                          "Cash Flow",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/icon/details.png',
+                          height: 40,
+                        ),
+                        const Text(
+                          "Cash Flow",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         ),
